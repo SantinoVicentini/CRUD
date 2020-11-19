cd96c8ed7ca4a3c608b8eea0adb2fc1d38392998
SantinoVicentini <vicen.lan2017@gmail.com>
2020-11-19 17:02:59 -0300

    modificacion terminada

--- ABM/controllers/productController.js
+++ ABM/controllers/productController.js
@@ -14,6 +14,34 @@ let indexController = {
         let productsJSON = JSON.stringify(products);
         fs.writeFileSync(__dirname + '/../data/productos.json', productsJSON);
         return res.send('Producto creado');
+    },
+    edit: function(req, res, next){
+        let productId = req.params.id;
+        let productFound;
+        for(let i = 0; i < products.length; i++){
+            if(products[i].id == productId){
+                productFound = products[i];
+                break;
+            }
+        }
+        if(productFound){
+            return res.render('edit', {productFound})
+        }
+        return res.send('no se encontro el producto ' + productId)
+    },
+    update: function(req,res,next){
+        let productId = req.params.id;
+        let editProduct = products.map(function(product){
+            if(product.id == productId){
+                let productoEditado = req.body;
+                productoEditado.id = productId;
+                return productoEditado;
+            }
+            return product;
+        });
+        editProductJSON = JSON.stringify(editProduct);
+        fs.writeFileSync(__dirname + '/../data/productos.json', editProductJSON);
+        return res.redirect('/edit/' + productId)
     }
 
 }
--- ABM/routes/products.js
+++ ABM/routes/products.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var productController = require('../controllers/productController')
 
 router.get('/', productController.home);
-router.get('/create', productController.create);
 
+router.get('/create', productController.create);
 router.post('/create', productController.store);
+
+router.get('/edit/:id', productController.edit);
+router.post('/edit/:id', productController.update);
 
 module.exports = router;
